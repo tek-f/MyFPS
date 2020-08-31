fe214055eaed499b2dac9a3f5ffe7302f29c3e91
tek-f <aaron.falco2@gmail.com>
2020-08-31 15:39:20 +1000

    Initial Commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float speed = 2f;
+    public float gravity = -10f;
+    public float jumpSpeed = 50f;
+    bool grounded;
+    bool firing;
+    float xRotation;
+    public Vector3 velocity;
+    Camera playerCamera;
+    Transform cameraTransform;
+    CharacterController charControl;
+    float groundedMOE = 1.1f;
+    public LayerMask groundLayerMask;
+    [Header("Shooting")]
+    float shotTS;
+    float fireDelay = 1.0f;
+    void MouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+    void PlayerMovement()
+    {
+        /*if(charControl.isGrounded && velocity.y < -2)
+        {
+            velocity.y = 0f;
+        }*/
+        grounded = Physics.Raycast(gameObject.transform.position, Vector3.down, groundedMOE, groundLayerMask);
+        if(grounded && velocity.y < -2)
+        {
+            velocity.y = 0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        charControl.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        charControl.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            Debug.Log("jump");
+            velocity.y += Mathf.Sqrt(jumpSpeed * -1 * gravity);
+        }
+    }
+    public void Shooting()
+    {
+        if(Time.time - shotTS > fireDelay)
+        {
+            shotTS = Time.time;
+            //Send out raycast/bullet
+            //if raycast//bullet hits player/enemy
+            //Play firing annimation
+            //  { do damage to player }
+        }
+    }
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        cameraTransform = playerCamera.transform;
+        charControl = gameObject.GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        MouseLook();
+        PlayerMovement();
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f, range = 100f;
+    public Camera fpsCamera;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public 
+    void Shoot()
+    {
+        if (muzzleFlash != null)
+        {
+            muzzleFlash.Play();
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("Fired");
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if(impactEffect != null)
+            {
+                GameObject Impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal), hit.transform);
+                Destroy(Impact, 10f);
+            }
+        }
+    }
+    private void Start()
+    {
+        if(fpsCamera == null)
+        {
+            fpsCamera = GetComponent<Camera>();
+        }
+        if (fpsCamera == null)
+        {
+            fpsCamera = Camera.main;
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+}
