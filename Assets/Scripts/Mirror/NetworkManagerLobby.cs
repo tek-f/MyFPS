a01e71befd23662e77a7468dc48b2c170fba94ea
tek-f <aaron.falco2@gmail.com>
2020-09-29 12:14:03 +1000

    Networking Updates

--- Assets/Scripts/Mirror/NetworkManagerLobby.cs
+++ Assets/Scripts/Mirror/NetworkManagerLobby.cs
@@ -1,18 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
 
-public class NetworkManagerLobby : MonoBehaviour
+namespace MyFPS.Mirror
 {
-    // Start is called before the first frame update
-    void Start()
+    public class NetworkManagerLobby : NetworkManager
     {
-        
-    }
+        [Scene] [SerializeField] private string MenuScene = string.Empty;
+        List<List<GameObject>> testlist = new List<List<GameObject>>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Room")]
+        [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+        public static event Action OnClientConnected;
+        public static event Action OnClientDisconnected;
+
+        public override void OnStartServer()
+        {
+            spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
+        }
+
+        public override void OnStartClient()
+        {
+            var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+
+            foreach (var prefab in spawnablePrefabs)
+            {
+                ClientScene.RegisterPrefab(prefab);
+            }
+        }
+
+        public override void OnClientDisconnect(NetworkConnection networkConnection)
+        {
+            base.OnClientDisconnect(networkConnection);
+
+            OnClientDisconnected?.Invoke();
+        }
+
+        public override void OnServerConnect(NetworkConnection networkConnection)
+        {
+            if(numPlayers >= maxConnections)
+            {
+                networkConnection.Disconnect();
+                return;
+            }
+            if(SceneManager.GetActiveScene().name != MenuScene)
+            {
+                NetworkRoomPlayerLobby roomPlayerLobby = Instantiate(roomPlayerPrefab);
+            }
+        }
+        public override void OnServerAddPlayer(NetworkConnection networkConnection)
+        {
+            if (SceneManager.GetActiveScene().name == MenuScene)
+            {
+                NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+                NetworkServer.AddPlayerForConnection(networkConnection, roomPlayerInstance.gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/Mirror/NetworkRoomPlayerLobby.cs
+++ Assets/Scripts/Mirror/NetworkRoomPlayerLobby.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine;
 
 namespace MyFPS.Mirror
 {
