6aecfaeee16558cbb7475d36c6c4b1f479a2e62f
tek-f <aaron.falco2@gmail.com>
2020-10-12 11:00:58 +1100

    Updates

---
+++ Assets/Scripts/Mirror/JoinLobbyMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace MyFPS.Mirror
+{
+    public class JoinLobbyMenu : MonoBehaviour
+    {
+        [SerializeField] private NetworkManagerLobby networkManager;
+
+        [Header("UI")]
+        [SerializeField] private GameObject landingPagePanel;
+        [SerializeField] private TMP_InputField isAddressInputField;
+        [SerializeField] private Button joinButton;
+    }
+}
---
+++ Assets/Scripts/Mirror/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyFPS.Mirror
+{
+    public class MainMenu : MonoBehaviour
+    {
+
+        [SerializeField] private NetworkManagerLobby networkManager;
+
+        [Header("IU")]
+        [SerializeField] private GameObject landingPagePanel;
+        public void HostLobby()
+        {
+            networkManager.StartHost();
+            landingPagePanel.SetActive(false);
+        }
+    }
+}
